a897d7b473d12a596139799c18cc34ac709b1c6b
Yusuke Arakawa <nekolaboratory@users.noreply.github.com>
2023-06-22 16:20:40 +0900

    Cache anyenv.

--- conf/zshrc.d/anyenv.zsh
+++ conf/zshrc.d/anyenv.zsh
@@ -1,7 +1,11 @@
 if [ -e $HOME/.anyenv ]; then
-    export ANYENV_ROOT="$HOME/.anyenv"
-    export PATH="$ANYENV_ROOT/bin:$PATH"
-    if command -v anyenv 1>/dev/null 2>&1; then
-        eval "$(anyenv init -)"
-    fi
+  if [ -z $TMUX ]; then
+    export PATH="$HOME/.anyenv/bin:$PATH"
+  fi
+
+  if ! [ -f /tmp/anyenv.cache ]; then
+    anyenv init - --no-rehash > /tmp/anyenv.cache
+    zcompile /tmp/anyenv.cache
+  fi
+  source /tmp/anyenv.cache
 fi

580a35d41a4ebc2e951d568c8e819a3472a521e3
Yusuke Arakawa <nekolaboratory@users.noreply.github.com>
2023-06-23 18:50:00 +0900

    Revert env cache.

--- conf/zshrc.d/anyenv.zsh
+++ conf/zshrc.d/anyenv.zsh
@@ -1,10 +1,7 @@
-if [ -e "$HOME/.anyenv" ]; then
-  export ANYENV_ROOT="$HOME/.anyenv"
-  export PATH="$ANYENV_ROOT/bin:$PATH"
-
-  if ! [ -f /tmp/anyenv.cache ]; then
-    anyenv init - --no-rehash > /tmp/anyenv.cache
-    zcompile /tmp/anyenv.cache
-  fi
-  source /tmp/anyenv.cache
+if [ -e $HOME/.anyenv ]; then
+    export ANYENV_ROOT="$HOME/.anyenv"
+    export PATH="$ANYENV_ROOT/bin:$PATH"
+    if command -v anyenv 1>/dev/null 2>&1; then
+        eval "$(anyenv init -)"
+    fi
 fi

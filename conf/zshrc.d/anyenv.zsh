4ecc92114dac72e10e0335604fcedbbbc11bae20
Yusuke Arakawa <nekolaboratory@users.noreply.github.com>
2023-06-26 10:39:00 +0900

    Cache anyenv.

--- conf/zshrc.d/anyenv.zsh
+++ conf/zshrc.d/anyenv.zsh
@@ -2,6 +2,6 @@ if [ -e $HOME/.anyenv ]; then
     export ANYENV_ROOT="$HOME/.anyenv"
     export PATH="$ANYENV_ROOT/bin:$PATH"
     if command -v anyenv 1>/dev/null 2>&1; then
-        eval "$(anyenv init -)"
+        eval "$(anyenv init - --no-rehash)"
     fi
 fi
